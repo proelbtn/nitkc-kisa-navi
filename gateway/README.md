27a6946eba3e0b5ab49652ed22e377441a54c037
proelbtn <proelbtn@users.noreply.github.com>
2018-11-30 16:41:14 +0900

    run sample code and check that the response is returned with either GET or POST

---
+++ gateway/README.md
@@ -0,0 +1,3 @@
+# gateway
+
+## schema for graphql
--- gateway/handlers/graphql.go
+++ gateway/handlers/graphql.go
@@ -4,14 +4,44 @@ import (
 	"net/http"
 	"github.com/labstack/echo"
 	"github.com/proelbtn/school-eve-navi/gateway/models"
+
+	"encoding/json"
+	"github.com/graphql-go/graphql"
 )
 
 func GraphQLHandler(c echo.Context) (err error) {
+	fields := graphql.Fields {
+		"hello": &graphql.Field {
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig {
+		Name: "RootQuery",
+		Fields: fields,
+	}
+
+	schemaConfig := graphql.SchemaConfig {
+		Query: graphql.NewObject(rootQuery),
+	}
+
+	schema, err := graphql.NewSchema(schemaConfig)
+
 	req := new(models.GraphQLRequest)
 
 	if err = c.Bind(req); err != nil {
 		return err
 	}
 
-	return c.String(http.StatusOK, req.Query)
+	params := graphql.Params {
+		Schema: schema,
+		RequestString: req.Query,
+	}
+
+	res, _ := json.Marshal(graphql.Do(params))
+
+	return c.String(http.StatusOK, string(res))
 }
