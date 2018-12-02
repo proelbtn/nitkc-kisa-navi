7edd35a8e52b7039cf34c38eee65e70b1621360c
proelbtn <proelbtn@users.noreply.github.com>
2018-12-02 17:59:23 +0900

    merge model files

--- gateway/models/query.go
+++ gateway/models/query.go
@@ -9,48 +9,111 @@ import (
 	"google.golang.org/grpc"
 )
 
+func GetFoodObject() *graphql.Object {
+	config := graphql.ObjectConfig{
+		Name: "Food",
+		Fields: graphql.Fields{
+			"name": &graphql.Field{
+				Type: graphql.String,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if obj, ok := p.Source.(*food.FoodResponse); ok {
+						return obj.Name, nil
+					}
+					return nil, errors.New("unable to cast")
+				},
+			},
+		},
+	}
+
+	return graphql.NewObject(config)
+}
+
+func GetShopObject() *graphql.Object {
+	config := graphql.ObjectConfig{
+		Name: "Shop",
+		Fields: graphql.Fields{
+			"name": &graphql.Field{
+				Type: graphql.String,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return nil, nil
+				},
+			},
+		},
+	}
+
+	return graphql.NewObject(config)
+}
+
+func GetSouvenirObject() *graphql.Object {
+	config := graphql.ObjectConfig{
+		Name: "Shop",
+		Fields: graphql.Fields{
+			"name": &graphql.Field{
+				Type: graphql.String,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return nil, nil
+				},
+			},
+		},
+	}
+
+	return graphql.NewObject(config)
+}
+
+func GetFoodField() *graphql.Field {
+	return &graphql.Field{
+		Args: graphql.FieldConfigArgument{
+			"name": &graphql.ArgumentConfig{
+				Type:         graphql.String,
+				DefaultValue: "Hello, World!",
+				Description:  "A name of food",
+			},
+		},
+		Type: GetFoodObject(),
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			name, ok := p.Args["name"].(string)
+			if !ok {
+				return nil, errors.New("name must be string")
+			}
+
+			cc, err := grpc.Dial("localhost:30001", grpc.WithInsecure())
+			if err != nil {
+				return nil, err
+			}
+
+			client := food.NewFoodClient(cc)
+			req := food.FoodRequest{Name: name}
+
+			return client.Search(context.Background(), &req)
+		},
+	}
+}
+
+func GetShopField() *graphql.Field {
+	return &graphql.Field{
+		Type: GetShopObject(),
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return nil, nil
+		},
+	}
+}
+
+func GetSouvenirField() *graphql.Field {
+	return &graphql.Field{
+		Type: GetShopObject(),
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return nil, nil
+		},
+	}
+}
+
 func GetQueryObject() *graphql.Object {
 	config := graphql.ObjectConfig{
 		Name: "Query",
 		Fields: graphql.Fields{
-			"foods": &graphql.Field{
-				Args: graphql.FieldConfigArgument{
-					"name": &graphql.ArgumentConfig{
-						Type:         graphql.String,
-						DefaultValue: "Hello, World!",
-						Description:  "A name of food",
-					},
-				},
-				Type: GetFoodObject(),
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					name, ok := p.Args["name"].(string)
-					if !ok {
-						return nil, errors.New("name must be string")
-					}
-
-					cc, err := grpc.Dial("localhost:30001", grpc.WithInsecure())
-					if err != nil {
-						return nil, err
-					}
-
-					client := food.NewFoodClient(cc)
-					req := food.FoodRequest{Name: name}
-
-					return client.Search(context.Background(), &req)
-				},
-			},
-			"shops": &graphql.Field{
-				Type: GetShopObject(),
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return Shop{Name: "Shop"}, nil
-				},
-			},
-			"souvenirs": &graphql.Field{
-				Type: GetSouvenirObject(),
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return Souvenir{Name: "Souvenir"}, nil
-				},
-			},
+			"foods":     GetFoodField(),
+			"shops":     GetShopField(),
+			"souvenirs": GetSouvenirField(),
 		},
 	}
 
